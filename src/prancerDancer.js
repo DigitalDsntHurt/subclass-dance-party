5896eff107271dbe8731cbf254ade8555ffe3246
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-12-01 14:29:54 -0800

    Implement button to add 10 dancers at once. Code and comments cleanup.

--- src/prancerDancer.js
+++ src/prancerDancer.js
@@ -1,24 +1,21 @@
 var PrancerDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('prancerDancer');
+  this.setWidth();
   this.glide();
+
 };
 
 PrancerDancer.prototype = Object.create(Dancer.prototype);
 PrancerDancer.prototype.constructor = PrancerDancer;
+PrancerDancer.prototype.setWidth = function() {
+  let randomWidth = 15 / 100 * $('body').width() * Math.random();
+  this.$node.css('width', randomWidth);
+};
 PrancerDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
   this.$node.toggle();
 };
 PrancerDancer.prototype.glide = function() {
-  // this.$node.animate({
-  //   opacity: 0.25,
-  //   left: '+=50',
-  //   height: 'toggle'
-  // }, 5000, function() {
-  //   // Animation complete.
-  // });
-
-  console.log(this);
 
 };
